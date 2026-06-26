83d892a2c62bee81409d105cc068ca1be26ec584
Renan Souza <renancodes@gmail.com>
2026-06-26 16:00:15 -0300

    ajuste para subir no github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/services-carsoul-page/",
   server: {
     host: "::",
     port: 8080,
